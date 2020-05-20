861d31a65a6a6154d5bd314ce8d4fa6c8f334ca4
Abhishek <kumarabhishek0388@gmail.com>
2020-05-20 12:31:55 +0530

    adding http call using axioms

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
 import NavBar from './components/layout/NavBar'
 import Users from './components/users/Users'
+// npm i axios for http apis
+import axios from 'axios'
 
 import './App.css';
 
 class App extends Component {
   
+  state = {
+    users : [],
+    loading : false
+  }
+  //lifecycle method
+  async componentDidMount() {
+    this.setState({loading : true});
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({users : res.data, loading : false})
+  }
+
+  //lifecycle method
   render() {
     return (
       <div className="App">
         <NavBar />
         <div className = 'container'>
-          <Users />  
+          <Users loading = {this.state.loading} users = {this.state.users}/>  
         </div>
       </div>
     );
