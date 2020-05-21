ed740a146adea1debe07560b9564af89b0473457
Abhishek <kumarabhishek0388@gmail.com>
2020-05-21 17:20:48 +0530

    conditional clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -21,6 +21,9 @@ export class Search extends Component {
                     onChange = {this.onChange}/>
                     <input type = "submit" value = "Search" className = "btn btn-dark btn-block"/>
                 </form>
+                {this.props.showClear && 
+                <button className = 'btn btn-light btn-block' onClick = {this.props.clearUsers}>Clear</button>
+                } 
             </div>
         )
     }
